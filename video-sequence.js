92f1266ba48dbc04d8facb49a25a5cff4eccf52b
Zagoman <lucaszago.ph@gmail.com>
2022-05-24 14:37:10 +0200

    fixing loop

--- video-sequence.js
+++ video-sequence.js
@@ -5,15 +5,23 @@ let videoId = 0;
 let incremental = 0;
 const vidAmount = 8;
 
+video.addEventListener("ended", () => {
+  if (!video.hasAttribute("loop")) {
+    nextVid();
+  }
+});
+
 buttonNext.addEventListener("click", nextVid);
 buttonPrev.addEventListener("click", prevVid);
 
 function nextVid() {
+  video.toggleAttribute("loop");
   incremental++;
   videoId = incremental % vidAmount;
   switchSource(videoId);
 }
 function prevVid() {
+  video.toggleAttribute("loop");
   incremental === 0 ? (incremental = vidAmount - 1) : incremental--;
   videoId = incremental % vidAmount;
   switchSource(videoId);
