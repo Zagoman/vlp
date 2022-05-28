57b822b5bb02d325a8d0ad991f0feef6a0129985
Panni <anna56c0@stud.kea.dk>
2022-05-28 18:13:37 +0200

    fadein burgir

--- script-burger.js
+++ script-burger.js
@@ -8,10 +8,12 @@ menuBtn.addEventListener("click", () => {
     if(!menuOpen && linksHidden) {
         menuBtn.classList.add("open");
         navLinks.classList.remove("hidden");
+        navLinks.classList.add("visible")
         menuOpen = true;
         linksHidden = false;
     } else {
         menuBtn.classList.remove("open");
+        navLinks.classList.remove("visible");
         navLinks.classList.add("hidden");
         menuOpen = false;
         linksHidden = true;
