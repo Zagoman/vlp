2fec9609ce9e4ef9ac26cac275fc30c9c6488ae6
Zagoman <lucaszago.ph@gmail.com>
2022-05-31 21:33:18 +0200

    video functionality working

--- video-sequence.js
+++ video-sequence.js
@@ -1,31 +1,56 @@
-const video = document.querySelector("#entry-video");
-const buttonNext = document.querySelector(".button-next");
-const buttonPrev = document.querySelector(".button-prev");
-let videoId = 0;
-let incremental = 0;
-const vidAmount = 8;
+const btnNext = document.querySelector(".button-next");
+const btnPrev = document.querySelector(".button-prev");
+const btnEnter = document.querySelector(".button-enter");
+const parent = document.querySelector(".hero-section");
+let canvas;
+let video;
+let videoSrcs = [];
+let curId = 0;
 
-video.addEventListener("ended", () => {
-  if (!video.hasAttribute("loop")) {
-    nextVid();
+function setup() {
+  canvas = createCanvas(window.innerWidth, window.innerHeight);
+  canvas.parent(parent);
+  background(110);
+  for (let i = 1; i <= 7; i++) {
+    videoSrcs.push(`${i}_video.mp4`);
   }
-});
+  video = createVideo("./media/" + videoSrcs[0], vidLoad);
+  video.hide();
+}
 
-buttonNext.addEventListener("click", nextVid);
-buttonPrev.addEventListener("click", prevVid);
-
-function nextVid() {
-  video.toggleAttribute("loop");
-  incremental++;
-  videoId = incremental % vidAmount;
-  switchSource(videoId);
+function draw() {
+  image(video, 0, 0, width, height);
+}
+function vidLoad() {
+  video.size(window.innerWidth, window.innerHeight);
+  video.mute = true;
+  if (!video.src.includes(videoSrcs[1])) {
+    video.volume(0);
+    video.loop();
+  } else {
+    video.play();
+    video.onended(nexVid);
+  }
+}
+function nexVid() {
+  if (curId < 6) {
+    curId++;
+  } else {
+    curId = 2;
+  }
+  video.remove();
+  video = createVideo("./media/" + videoSrcs[curId], vidLoad);
+  video.hide();
 }
 function prevVid() {
-  video.toggleAttribute("loop");
-  incremental === 0 ? (incremental = vidAmount - 1) : incremental--;
-  videoId = incremental % vidAmount;
-  switchSource(videoId);
+  if (curId > 2) {
+    curId--;
+  } else {
+    curId = 6;
+  }
+  video.remove();
+  video = createVideo("./media/" + videoSrcs[curId], vidLoad);
+  video.hide();
 }
-function switchSource(id) {
-  video.src = `media/${id}_sequence.mp4`;
-}
+btnPrev.addEventListener("click", prevVid);
+btnNext.addEventListener("click", nexVid);
