64bf809612d7e42ec2370ae4249354cd754ee3f4
Zagoman <lucaszago.ph@gmail.com>
2022-06-01 16:51:42 +0200

    finished marquee

---
+++ index.js
@@ -0,0 +1,4 @@
+const title = document.querySelector("#about-title");
+const marquee1 = document.querySelector("#about-marquee");
+const marquee2 = document.querySelector("#about-marquee2");
+const aboutMarquee = new Marquee(title, marquee1, marquee2);
