13f8204c3647c2f5a8dcf82741eb6e6fffe09af0
ReiSikk <reix0010@stud.kea.dk>
2022-06-02 14:46:09 +0200

    added marquees

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const title = document.querySelector("#about-title");
 const marquee1 = document.querySelector("#about-marquee");
 const marquee2 = document.querySelector("#about-marquee2");
 const aboutMarquee = new Marquee(title, marquee1, marquee2);
+
+// About page marquee starts///
+
+////About page marquee ends///
