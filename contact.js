553d485343b247d027ce3f7e6ac3cb606cb62d6b
Zagoman <lucaszago.ph@gmail.com>
2022-06-03 12:26:58 +0200

    add contact.js

---
+++ contact.js
@@ -0,0 +1,5 @@
+const findTitle = document.querySelector("#find-title");
+const findMarquee1 = document.querySelector("#find-marquee1");
+const findMarquee2 = document.querySelector("#find-marquee2");
+
+const findMarquee = new Marquee(findTitle, findMarquee1, findMarquee2);
