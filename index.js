64f8d7fc8788d8a628f90ba7df63084abbb39b4f
Zagoman <lucaszago.ph@gmail.com>
2022-06-03 15:06:26 +0200

    homepage without past events slider

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const marquee1 = document.querySelector("#about-marquee");
 const marquee2 = document.querySelector("#about-marquee2");
 const aboutMarquee = new Marquee(title, marquee1, marquee2);
 
-// About page marquee starts///
-
-////About page marquee ends///
+const pastTitle = document.querySelector("#past-title");
+const pastMarquee1 = document.querySelector("#past-marquee");
+const pastMarquee2 = document.querySelector("#past-marquee2");
+const pastMarquee = new Marquee(pastTitle, pastMarquee1, pastMarquee2);
