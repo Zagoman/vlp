6241db3b89d8532c37a4084612c8349af8601cda
Hubert <ziemnioczek@gmail.com>
2022-06-04 12:15:38 +0200

    fixes

--- single_artwork.js
+++ single_artwork.js
@@ -1,6 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
-const url = `https://lucaszago.dk/vlp/wp-json/wp/v2/artworks`;
-const query = urlParams.get("category");
+const query = urlParams.get("id");
+const url = `https://lucaszago.dk/vlp/wp-json/wp/v2/artworks?include=${query}`;
 
 fetch(url)
   .then((response) => {
@@ -18,37 +18,28 @@ fetch(url)
   });
 
 function showProduct(product) {
-  // grab the template\
-  const template = document.querySelector("#template").content;
-  // clone the template
-  const copy = template.cloneNode(true);
-  // change content
-
-  copy.querySelector(".artworks_piece_name").textContent =
-    product.title.rendered;
-  copy.querySelector("#artworks_artist").textContent = product.artists;
-  copy.querySelector("#artworks_year").textContent = product.year_created;
-
-  copy.querySelector("img").src = product.images[0].guid;
-  copy
-    .querySelector("a")
-    .setAttribute("href", `single_artwork.html?id=${product.id}`);
-
+  // // grab the template\
+  // const template = document.querySelector("#template2").content;
+  // // clone the template
+  // const copy = template.cloneNode(true);
+  // // change content
+  document.querySelector("#title_single_art").textContent = product.origin;
+  document.querySelector("#artists_name_single_art").textContent =
+    product.materials;
+  // copy
+  //   .querySelector("a")
+  //   .setAttribute("href", `single_artwork.html?id=${product.id}`);
   //   copy
   //     .querySelector(".bagname")
   //     .setAttribute("href", `product_view.html?id=${product.id}`);
   //   copy.querySelector(".price").textContent = "DKK " + product.price + ",00";
-
   //   copy.querySelector(".hoverImg").src = product.image.guid;
-
   //   console.log("yes");
-
   //   copy
   //     .querySelector("a")
   //     .setAttribute("href", `product_view.html?id=${product.id}`);
-
   // grab parent
-  const parent = document.querySelector(".artworks_grid");
-  // append
-  parent.appendChild(copy);
+  // const parent = document.querySelector(".artworks_grid");
+  // // append
+  // parent.appendChild(copy);
 }
