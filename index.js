e2ebfb6af7acac9ec0ddf53249ba5ae6feae59c5
Zagoman <lucaszago.ph@gmail.com>
2022-06-06 21:51:08 +0200

    fetch data on homepage

--- index.js
+++ index.js
@@ -27,7 +27,12 @@ document.addEventListener("DOMContentLoaded", dataLoad);
 async function dataLoad() {
   const response = await fetch("https://lucaszago.dk/vlp/wp-json/wp/v2/event");
   const data = await response.json();
+  const responseEx = await fetch(
+    "https://lucaszago.dk/vlp/wp-json/wp/v2/exhibitions"
+  );
+  const dataEx = await responseEx.json();
   dataAppend(data);
+  exhibitionAppend(dataEx);
 }
 function dataAppend(data) {
   console.log(data);
@@ -48,4 +53,43 @@ function dataAppend(data) {
   });
 
   splide.mount();
+}
+
+/*
+
+<template id="curExTemp">
+        <div class="container">
+          <h2 class="fs_largest">Tak Tak</h2>
+          <span class="on-view">On View</span>
+          <span class="view-dates">29.04.2022.-13.05.2022.</span>
+          <span class="curated-tag">Curated by Marta Czyż and Vi Lever På Polsk</span>
+          <a href="./exhibition.html" class="button">View the exhibition</a>
+          <img src="./media/texture1.png" alt="texture" class="deco-texture">
+        </div>
+        <img src="./media/gallery-image.jpg" alt="Exhibition Poster">
+      </template>
+
+
+*/
+
+function exhibitionAppend(data) {
+  let bool = false;
+  const parent = document.querySelector(".exhibition-wrapper");
+  const temp = document.querySelector("#curExTemp").content;
+  const clone = temp.cloneNode(true);
+  data.forEach((el) => {
+    if (Number(el["on-view"])) {
+      clone.querySelector("h2").textContent = el.title.rendered;
+      clone.querySelector(".curated-tag").textContent = el["curated_by"];
+      clone.querySelector(
+        ".view-dates"
+      ).textContent = `${el["start-date"]} - ${el["end-date"]}`;
+
+      parent.append(clone);
+      bool = true;
+    }
+  });
+  if (!bool) {
+    document.querySelector("#curExhibition").remove();
+  }
 }
